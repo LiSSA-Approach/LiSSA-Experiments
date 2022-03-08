3cfe609b670222fa41fc5ccbe5087aa14e131e7c
Dominik Fuchß <dominik.fuchss@kit.edu>
2022-03-08 12:54:22 +0100

    Added missing require

--- linking/src/main/kotlin/edu/kit/kastel/lissa/linking/graphmatcher/Graph.kt
+++ linking/src/main/kotlin/edu/kit/kastel/lissa/linking/graphmatcher/Graph.kt
@@ -9,7 +9,7 @@ fun convertSketchToGraph(sketch: Sketch): ListenableGraph<String, String> {
     val result: ListenableGraph<String, String> = DefaultListenableGraph(DefaultDirectedGraph(String::class.java))
     sketch.getBoxElements().forEach { result.addVertex(it.name()) }
     sketch.getRelationElements().map { it to it.connectedElements() }
-        .also { list -> list.all { (_, elements) -> elements.size == 2 } }
+        .also { list -> require(list.all { (_, elements) -> elements.size == 2 }) }
         .forEach { result.addEdge(it.second[0].name(), it.second[1].name(), it.first.name()) }
     return result
 }
