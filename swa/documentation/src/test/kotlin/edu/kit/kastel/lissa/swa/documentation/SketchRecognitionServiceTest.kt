a76bb987f5f24a57c36290265011a005f65a2889
Dominik Fuchß <dominik.fuchss@kit.edu>
2022-06-07 17:03:33 +0300

    Added BBB

--- swa/documentation/src/test/kotlin/edu/kit/kastel/lissa/swa/documentation/SketchRecognitionServiceTest.kt
+++ swa/documentation/src/test/kotlin/edu/kit/kastel/lissa/swa/documentation/SketchRecognitionServiceTest.kt
@@ -12,6 +12,11 @@ import java.io.FileOutputStream
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class SketchRecognitionServiceTest {
+    companion object {
+        const val PATH = "src/test/resources/bbb-arch-overview.png"
+        // const val PATH = "src/test/resources/highlevelArchitecture.png"
+    }
+
     private lateinit var service: SketchRecognitionService
 
     @BeforeAll
@@ -28,14 +33,14 @@ class SketchRecognitionServiceTest {
 
     @Test
     fun testSimpleRecognition() {
-        val file = FileInputStream(File("src/test/resources/highlevelArchitecture.png"))
+        val file = FileInputStream(File(PATH))
         val response = service.recognize(file)
         Assertions.assertNotNull(response)
-        Assertions.assertEquals(42, response.boxes.size)
+        // Assertions.assertEquals(42, response.boxes.size)
 
         val destination = File("target/testout/result_testSimpleRecognition.png")
         visualize(
-            FileInputStream(File("src/test/resources/highlevelArchitecture.png")),
+            FileInputStream(File(PATH)),
             response,
             FileOutputStream(destination)
         )
--- swa/documentation/src/test/resources/LICENSE.md
+++ swa/documentation/src/test/resources/LICENSE.md
@@ -1,5 +1,11 @@
 The files in this directory are not covered by the LICENSE file in the root directory of the project.
 
 # Sources
-## GPL 2.0
+
+## highlevelArchitecture
+
 * https://github.com/TEAMMATES/teammates/blob/master/docs/images/highlevelArchitecture.png
+
+## bbb-arch-overview
+
+* https://docs.bigbluebutton.org/2.4/architecture.html
